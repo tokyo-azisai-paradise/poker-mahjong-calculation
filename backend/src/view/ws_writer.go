b20a6e0a38622c5377b0da3bbfed448d2e25a77a
Kyosuke Fujita <kurofuku01@gmail.com>
2022-10-12 11:31:39 +0900

    不要な関数削除

--- backend/src/view/ws_writer.go
+++ backend/src/view/ws_writer.go
@@ -5,17 +5,6 @@ import (
 	"pms/src/model"
 )
 
-func WriteJSON(pr *model.PokerRoom) {
-	users := pr.Users
-	for uid, u := range users {
-		if err := u.WsConn.WriteJSON(pr); err != nil {
-
-			users[uid].WsConn = nil
-			users[uid].SessionAlive = false
-		}
-	}
-}
-
 func WriteRoomInfoByWS(pr *model.PokerRoom, u *model.User) error {
 	conn := u.WsConn
 	if err := conn.WriteJSON(pr); err != nil {
