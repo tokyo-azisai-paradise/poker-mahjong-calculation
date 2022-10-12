33fefdce8603c12d8db0f85c1d04c312d8d6be31
Kyosuke Fujita <kurofuku01@gmail.com>
2022-10-12 14:58:33 +0900

    軽微な修正
    view.WriteRoomInfobyWsを修正

--- backend/src/view/ws_writer.go
+++ backend/src/view/ws_writer.go
@@ -1,12 +1,12 @@
 package view
 
 import (
-	// "github.com/gorilla/websocket"
+	"github.com/gorilla/websocket"
 	"pms/src/model"
 )
 
-func WriteRoomInfoByWS(pr *model.PokerRoom, u *model.User) error {
-	conn := u.WsConn
+// User.WsConnにWriteJSONでRoomInfoを送信する
+func WriteRoomInfobyWS(conn *websocket.Conn, pr *model.PokerRoom) error {
 	if err := conn.WriteJSON(pr); err != nil {
 		return err
 	} else {
